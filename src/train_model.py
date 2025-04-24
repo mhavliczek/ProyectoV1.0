bc4a6fbfd07c493f3aa1fc25564e28b056e5d0e2
mhavliczek <mhavliczek@gmail.com>
2025-04-23 20:48:14 -0400

    cambios en el train model

--- src/train_model.py
+++ src/train_model.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report
 import joblib
 
 # Paso 1: Cargar los datos
-df = pd.read_csv("../data/datos_generados.csv")
+df = pd.read_csv("data/datos_generados.csv")
 
 # Paso 2: Preprocesamiento
 # Codificar variables categóricas
@@ -28,5 +28,5 @@ print("Reporte de Clasificación:")
 print(classification_report(y_test, y_pred))
 
 # Paso 4: Guardar el modelo entrenado
-joblib.dump(model, "../data/modelo_entrenado.joblib")
+joblib.dump(model, "data/modelo_entrenado.joblib")
 print("Modelo entrenado y guardado en 'data/modelo_entrenado.joblib'")
